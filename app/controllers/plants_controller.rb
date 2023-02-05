8422f50442db8fe84557f867655d3ac4153063ea
Theodore John Josef <john.josef@aol.com>
2023-02-05 01:02:16 -0800

    commit tree 3524471075b14fcb632a82f6eab05631ee44a780

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,7 +18,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = find_by_id
+    if plant
+      plant.update(plant_params)
+      render json: plant 
+    else 
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def destroy 
+    plant = find_by_id 
+    if plant 
+      plant.destroy 
+      head :no_content 
+    else 
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
   private
+
+  def find_by_id
+    Plant.find_by_id(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
